bf8ca1d2993f288f48c359db393e469b268c6801
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-09-23 16:23:51 +0300

    update: apps, models, setting

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
 
 # Register your models here.
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название товара')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание товара')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
