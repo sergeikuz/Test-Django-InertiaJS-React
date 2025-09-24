685629e7206a51fee6e855914ec2b895cd4a8d77
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-09-24 13:22:08 +0300

    update: settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_vite',
-    'backend.app',
     'inertia',
+    'backend.app',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
